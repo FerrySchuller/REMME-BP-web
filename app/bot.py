b5e8eae065df97ab76840c3c7a4d0f7cf0aedb73
FerrySchuller <ferry@f-inter.net>
2019-10-12 10:49:49 +0200

    commit by make git

--- app/bot.py
+++ app/bot.py
@@ -1,5 +1,1 @@
 # curl -s "https://data.gateio.co/api2/1/candlestick2/rem_usdt?group_sec=60&range_hour=1" | python -m json.tool > rem_usdt_ohlc
-
-import plotly.graph_objects as go
-fig = go.Figure(data=go.Bar(y=[2, 3, 1]))
-fig.write_html('first_figure.html', auto_open=True)
