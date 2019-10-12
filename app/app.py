711b74a30c27c5f0fea250222e7518eaa6adf9b7
FerrySchuller <ferry@f-inter.net>
2019-10-12 10:53:27 +0200

    commit by make git

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_assets import Environment, Bundle
 
 app = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('/prod/apps/remme/REMME-BP-web/app/config.py')
+app.config.from_pyfile('app/config.py')
 assets = Environment(app)
 
 
