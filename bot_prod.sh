e39931e44c1bf299a64a619d017f4ee214e415d8
FerrySchuller <ferry@f-inter.net>
2020-02-17 07:55:48 +0000

    commit by make git

--- bot_prod.sh
+++ bot_prod.sh
@@ -5,7 +5,13 @@ case $1 in
       /prod/REMME-BP-web/env/bin/python /prod/REMME-BP-web/app/bot.py monit &
       ;;
     stop)  
-      kill `cat /prod/REMME-BP-web/bot.pid` ;;
+      kill `cat /prod/REMME-BP-web/bot.pid` 
+      ;;
+    restart)
+      kill `cat /prod/REMME-BP-web/bot.pid` 
+      sleep 1
+      /prod/REMME-BP-web/env/bin/python /prod/REMME-BP-web/app/bot.py monit &
+      ;;
     *)  
       echo "usage: bot.sh {start|stop}" ;;
 esac
